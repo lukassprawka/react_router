28db5628df090a8aed0bc598420bee55721be912
Lukasz Sprawka <lukas.sprawka@gmail.com>
2019-12-17 10:55:56 +0100

    Updates

---
+++ precache-manifest.e68e598464863ceb0e45f174332fcd20.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3375de67010cc4ab4482546eeb613e1",
+    "url": "/react_router/index.html"
+  },
+  {
+    "revision": "077193d2ba58e912ab21",
+    "url": "/react_router/static/css/main.2a7d75a5.chunk.css"
+  },
+  {
+    "revision": "550c746b4e15ee6aa7ff",
+    "url": "/react_router/static/js/2.6fb5f7d9.chunk.js"
+  },
+  {
+    "revision": "cf67cabbb2d2632104356310a9e06d11",
+    "url": "/react_router/static/js/2.6fb5f7d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "077193d2ba58e912ab21",
+    "url": "/react_router/static/js/main.f0c3cd0c.chunk.js"
+  },
+  {
+    "revision": "84434461667a1c2510e8",
+    "url": "/react_router/static/js/runtime-main.c54e7cc9.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react_router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react_router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react_router/static/media/header3.d9d36b65.jpg"
+  }
+]);
